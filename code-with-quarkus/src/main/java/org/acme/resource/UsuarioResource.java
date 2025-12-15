2f66d4cdfa1035f3e894e523ac587a9f82a63fae
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-12-14 22:35:43 -0300

    COlocando annotations no resources

--- code-with-quarkus/src/main/java/org/acme/resource/UsuarioResource.java
+++ code-with-quarkus/src/main/java/org/acme/resource/UsuarioResource.java
@@ -1,7 +1,10 @@
 package org.acme.resource;
 
 import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.acme.service.UsuarioService;
 
@@ -13,6 +16,8 @@ public class UsuarioResource {
     @Inject
     UsuarioService usuarioService;
 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     public Response listarUsuario(){
 
         try{
