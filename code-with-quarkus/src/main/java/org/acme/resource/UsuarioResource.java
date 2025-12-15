7b350b72a647c7f1db65e11904060d6515cf62de
Eduardo-Locaspi <dudulocaspi@gmail.com>
2025-12-14 22:23:46 -0300

    Update UsuarioResource.java

--- code-with-quarkus/src/main/java/org/acme/resource/UsuarioResource.java
+++ code-with-quarkus/src/main/java/org/acme/resource/UsuarioResource.java
@@ -1,8 +1,25 @@
 package org.acme.resource;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+import org.acme.service.UsuarioService;
+
+import java.sql.SQLException;
 
 @Path("user")
 public class UsuarioResource {
-    
+
+    @Inject
+    UsuarioService usuarioService;
+
+    public Response listarUsuario(){
+
+        try{
+            return Response.ok(usuarioService.listarUsuarios()).build();
+        }catch (SQLException e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Erro de Banco de Dados").build();
+        }
+
+    }
 }
